b0272430d2aa4ed4710d70d2d7a59fc4f0204037
Nachiket Roy <nachiket.roy.2@gmail.com>
2026-05-17 23:29:21 +0300

    feat: implement composable normalizers for timestamps, request IDs, UUIDs, and cursors with configuration support

--- mcp_vcr/__init__.py
+++ mcp_vcr/__init__.py
@@ -2,6 +2,14 @@ from .schema import Transcript, Message, Metadata, Direction
 from .validator import validate_file, validate_transcript
 from .interceptor import MessageInterceptor
 from .redactor import Redactor
+from .normalizer import (
+    Normalizer,
+    NormalizerChain,
+    TimestampNormalizer,
+    RequestIdNormalizer,
+    UuidNormalizer,
+    CursorNormalizer
+)
 from .transport import (
     get_stdin_reader,
     launch_server,
@@ -20,6 +28,12 @@ __all__ = [
     "validate_transcript",
     "MessageInterceptor",
     "Redactor",
+    "Normalizer",
+    "NormalizerChain",
+    "TimestampNormalizer",
+    "RequestIdNormalizer",
+    "UuidNormalizer",
+    "CursorNormalizer",
     "get_stdin_reader",
     "launch_server",
     "pump_c2s",
